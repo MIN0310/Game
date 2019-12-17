5b847bf22aebd70aa016c16c1b95acb7f4a4f785
miia <miia@com.tw>
2019-12-17 16:02:35 +0800

    按不同鍵會用同個指令/加上下左右鍵

---
+++ app/src/main/java/com/min/game/MainActivity.java
@@ -0,0 +1,43 @@
+package com.min.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews() {
+        findViewById(R.id.button_up).setOnClickListener(this);
+        findViewById(R.id.button_down).setOnClickListener(this);
+        findViewById(R.id.button_left).setOnClickListener(this);
+        findViewById(R.id.button_right).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_up:
+                Log.d("MainActivity", "onClick: UP");
+                break;
+            case R.id.button_down:
+                Log.d("MainActivity", "onClick: DOWN");
+                break;
+            case R.id.button_left:
+                Log.d("MainActivity", "onClick: LEFT");
+                break;
+            case R.id.button_right:
+                Log.d("MainActivity", "onClick: RIGHT");
+                break;
+        }
+
+    }
+}
